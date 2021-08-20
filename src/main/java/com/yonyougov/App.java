b5c2d17db26fbfc389bfef0aa41763114f6641a7
yindwe <yindwe@yonyou.com>
2021-08-20 16:59:20 +0800

    1

--- src/main/java/com/yonyougov/App.java
+++ src/main/java/com/yonyougov/App.java
@@ -1,17 +1,12 @@
 package com.yonyougov;
 
-import com.yonyougov.ptp.core.config.TransactionManagerConfig;
-import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Hello world!
  */
 @SpringBootApplication
-@EnableSwagger2
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
